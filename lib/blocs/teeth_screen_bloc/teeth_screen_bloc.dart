88eacd50c13e6d390bf59c1965c752bce8d4d9e9
juman-m <juman.m1999@gmail.com>
2023-12-30 23:50:15 +0300

    delete tooth status is completed

--- lib/blocs/teeth_screen_bloc/teeth_screen_bloc.dart
+++ lib/blocs/teeth_screen_bloc/teeth_screen_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
+import 'package:final_project/models/tooth_model.dart';
 import 'package:final_project/screens/teeth_screens/teeth_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 part 'teeth_screen_event.dart';
 part 'teeth_screen_state.dart';
@@ -11,6 +13,46 @@ class TeethScreenBloc extends Bloc<TeethScreenEvent, TeethScreenState> {
       emit(TeethScreenLoadingState());
       List<Color> teethColors = await colorStatus();
       emit(TeethScreenColorsState(teethColors));
+    });
+
+    on<TeethStatusDeleteEvent>((event, emit) async {
+      emit(TeethDeleteLoadingState());
+      try {
+        final supabase = Supabase.instance.client;
+        final userId = supabase.auth.currentUser!.id;
+
+        final response = await supabase
+            .from("teeth_status")
+            .select('*')
+            .match({'user_id': userId, "tooth_no": event.toothNum});
+
+        final Tooth tooth = Tooth.fromJson(response[0]);
+        if (tooth.report != "") {
+          await supabase.storage
+              .from('ToothImage')
+              .remove(['$userId@${event.toothNum}@report.png']);
+        }
+
+        if (tooth.prescription != "") {
+          await supabase.storage
+              .from('ToothImage')
+              .remove(['$userId@${event.toothNum}@prescription.png']);
+        }
+        if (tooth.xray != "") {
+          await supabase.storage
+              .from('ToothImage')
+              .remove(['$userId@${event.toothNum}@xRay.png']);
+        }
+
+        await supabase
+            .from('teeth_status')
+            .delete()
+            .match({'user_id': userId, "tooth_no": event.toothNum});
+        emit(TeethStatusDeleteState());
+      } catch (e) {
+        emit(ToothDeleteErrorState("حدث خطأ ما !"));
+        print(e);
+      }
     });
   }
 }
--- lib/services/supabase_teeth_request.dart
+++ lib/services/supabase_teeth_request.dart
@@ -11,6 +11,39 @@ updateToothStatus({required Map body, required String id}) async {
   await supabase.from('teeth_status').update(body).eq("id", id);
 }
 
+// deleteToothStatus({required String toothNum}) async {
+//   final supabase = Supabase.instance.client;
+//   final userId = supabase.auth.currentUser!.id;
+
+//   final response = await supabase
+//       .from("teeth_status")
+//       .select('*')
+//       .match({'user_id': userId, "tooth_no": toothNum});
+
+//   final Tooth tooth = Tooth.fromJson(response[0]);
+//   if (tooth.report != "") {
+//     await supabase.storage
+//         .from('ToothImage')
+//         .remove(['$userId@$toothNum@report.png']);
+//   }
+
+//   if (tooth.prescription != "") {
+//     await supabase.storage
+//         .from('ToothImage/prescription')
+//         .remove(['$userId@$toothNum@prescription.png']);
+//   }
+//   if (tooth.xray != "") {
+//     await supabase.storage
+//         .from('ToothImage')
+//         .remove(['$userId@$toothNum@xRay.png']);
+//   }
+
+//   await supabase
+//       .from('teeth_status')
+//       .delete()
+//       .match({'user_id': userId, "tooth_no": toothNum});
+// }
+
 Future<Tooth?> getToothStatusById(String userId, String toothNo) async {
   final supabase = Supabase.instance.client;
   final response = await supabase
